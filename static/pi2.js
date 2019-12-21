a8f9aa1bb302485adbaadc14cdb32c9656ca686a
Yifan - Macbook Pro <zyfinori@outlook.com>
2019-12-21 15:51:38 +0800

    Modify pi2.js

--- static/pi2.js
+++ static/pi2.js
@@ -1,7 +1,7 @@
+const pi = 3.1415926535;
+
 function pi2_click_start() {
-    var pi = 3.1415926535;
-    var f = document.getElementById("pi2-work-freq").value;
-    var l = document.getElementById("pi2-l").value;
+    let f = parseFloat($("#pi2-work-freq").val()), l;
     if (f >= 1 && f < 2) {
         l = 21;
     }
@@ -44,50 +44,49 @@ function pi2_click_start() {
     if (f >= 133) {
         alert("Illegal input!");
     }
-    var l = document.getElementById("pi2-l").value = l;
-    var w = 2 * pi * f * 1000000;
-    var c3 = 0.01 / w;
-    document.getElementById("pi2-c3").value = c3 * 1000000000000;
-    var ll = l / 1000000.0;
-    var rmin = 1.0,
+    // 更新建议的电感L值
+    $("#pi2-l").val(l);
+    // 工作角频率为w
+    let w = 2 * pi * f * 1000000;
+    let c3 = 0.01 / w;
+    // 设置默认的C3参数，单位为pF
+    $("#pi2-c3").val((c3 * 1000000000000).toFixed(2));
+    let ll = l / 1000000.0; // ll单位为H
+    // 能匹配的阻抗范围
+    let rmin = 1.0,
         rmax = 49.0,
         xmin = -100.0,
         xmax = 1000.0;
-    document.getElementById("pi2-c1-upper").value = Math.sqrt(0.02 / rmin - 0.02 * 0.02) / w * 1000000000000;
-    document.getElementById("pi2-c1-lower").value = Math.sqrt(0.02 / rmax - 0.02 * 0.02) / w * 1000000000000;
-    var d1 = Math.floor(rmax - rmin),
+    // 设置c1的建议最小值和建议最大值
+    $("#pi2-c1-upper").val(Math.sqrt((0.02 / rmin - 0.02 * 0.02) / w * 1000000000000).toFixed(2));
+    $("#pi2-c1-lower").val(Math.sqrt((0.02 / rmax - 0.02 * 0.02) / w * 1000000000000).toFixed(2));
+    let d1 = Math.floor(rmax - rmin),
         d2 = Math.floor((xmax - xmin) / 5);
-    var c2 = new Array();
-    for (var i = 0; i < d1; ++i) {
-        c2[i] = new Array();
-        for (var j = 0; j < d2; ++j) {
+    let c2 = [];
+    for (let i = 0; i < d1; ++i) {
+        c2[i] = [];
+        for (let j = 0; j < d2; ++j) {
             c2[i][j] = 0;
         }
     }
-    var rrr = new Array();
-    var tempp = 0;
-    for (var i = rmin; i < rmax; i++) {
-        var s11 = 50 * (i) * Math.sqrt(0.02 / (i) - 0.02 * 0.02) * w;
-        rrr[tempp] = s11;
-        ++tempp;
+    let rrr = [];
+    for (let i = rmin, tempp = 0; i < rmax; i++, tempp++) {
+        rrr[tempp] = 50 * (i) * Math.sqrt(0.02 / (i) - 0.02 * 0.02) * w;
     }
-    var xxx = new Array();
-    tempp = 0;
-    for (var i = xmin; i < xmax; i += 5) {
-        var y11 = (i + w * ll) * w;
-        xxx[tempp] = y11;
-        ++tempp;
+    let xxx = [];
+    for (let i = xmin, tempp = 0; i < xmax; i += 5, tempp++) {
+        xxx[tempp] = (i + w * ll) * w;
     }
-    for (var i = 0; i < d1; i++) {
-        for (var j = 0; j < d2; j++) {
-            c2[i][j] = 1.0 * 1000000000000 / (xxx[j] - rrr[i]);
+    for (let i = 0; i < d1; i++) {
+        for (let j = 0; j < d2; j++) {
+            c2[i][j] = 1000000000000 / (xxx[j] - rrr[i]);
 
         }
     }
-    var c2min = c2[0][0],
+    let c2min = c2[0][0],
         c2max = c2[0][0];
-    for (var i = 0; i < d1; i++) {
-        for (var j = 0; j < d2; j++) {
+    for (let i = 0; i < d1; i++) {
+        for (let j = 0; j < d2; j++) {
             if (c2[i][j] < c2min) {
                 c2min = c2[i][j];
             }
@@ -96,107 +95,97 @@ function pi2_click_start() {
             }
         }
     }
-    document.getElementById("pi2-c2-lower").value = c2min;
-    document.getElementById("pi2-c2-upper").value = c2max;
+    // 更新c2的建议最小值和最大值
+    $("#pi2-c2-lower").val(c2min.toFixed(2));
+    $("#pi2-c2-upper").val(c2max.toFixed(2));
 }
 
-function pi2_calculateRRange(l, c3) {
-    var pi = 3.1415926535;
-    //var l=document.getElementById("pi2-l").value/1000000.0;
-    var f = document.getElementById("pi2-work-freq").value * 1000000.0;
-    var c1min = document.getElementById("pi2-c1-lower").value;
-    var c1max = document.getElementById("pi2-c1-upper").value;
-    var c2min = document.getElementById("pi2-c2-lower").value;
-    var c2max = document.getElementById("pi2-c2-upper").value;
-    var w = 2 * pi * f;
-    //var c3=0.01/w;
-    var a = new Complex(1, 0);
-    var yloadg = new Complex(0.02, 0);
-    var zloadg = new Complex(0, 0);
-    var d = Math.floor(c1max - c1min);
-    var complex_array = new Array();
+function pi2_calculateRRange(l) {
+    let pi = 3.1415926535;
+    let f = $("#pi2-work-freq").val() * 1000000.0;
+    let c1min = parseFloat($("#pi2-c1-lower").val());
+    let c1max = parseFloat($("#pi2-c1-upper").val());
+    let c2min = parseFloat($("#pi2-c2-lower").val());
+    let c2max = parseFloat($("#pi2-c2-upper").val());
+    let a = new Complex(1, 0);
+    let yloadg = new Complex(0.02, 0);
+    let d = Math.floor(c1max - c1min);
+    let complex_array = [];
 
 
     complex_array[0] = new Complex(0,0);
-    var ci = c1min;
-    for (var i = 0; i < d; i++) {
+    let ci = c1min;
+    for (let i = 0; i < d; i++) {
         yloadg.i = 2 * pi * f * ci / 1000000000000;
         complex_array[i] = complexDivide(a, yloadg);
         ci++;
     }
 
-    var rmax = complex_array[0].r;
-    var rmin = complex_array[0].r;
+    let rmax = complex_array[0].r;
+    let rmin = complex_array[0].r;
 
-    for (var i = 0; i < d; i++) {
+    for (let i = 0; i < d; i++) {
         if (complex_array[i].r > rmax)
             rmax = complex_array[i].r;
         if (complex_array[i].r < rmin)
             rmin = complex_array[i].r;
     }
-    document.getElementById("pi2-r-real-lower").value = rmin;
-    document.getElementById("pi2-r-real-upper").value = rmax;
+    $("#pi2-r-real-lower").val(rmin.toFixed(2));
+    $("#pi2-r-real-upper").val(rmax.toFixed(2));
 
 
-    var xpos = new Array();
-    var xneg = new Array();
-    for (var i = 0; i < d; i++) {
+    let xpos = [];
+    let xneg = [];
+    for (let i = 0; i < d; i++) {
         xpos[i] = complex_array[i].i + 2 * pi * f * l - 1 / (c2min * 2 * pi * f / 1000000000000);
         xneg[i] = complex_array[i].i + 2 * pi * f * l - 1 / (c2max * 2 * pi * f / 1000000000000);
     }
 
-    var xmax = xpos[0];
-    var xmin = xneg[0];
-    for (var i = 0; i < d; i++) {
+    let xmax = xpos[0];
+    let xmin = xneg[0];
+    for (let i = 0; i < d; i++) {
         if (xmax < xpos[i]) xmax = xpos[i];
         if (xmin > xneg[i]) xmin = xneg[i];
     }
     xmax = 0 - xmax;
     xmin = 0 - xmin;
-    document.getElementById("pi2-r-imag-lower").value = xmin;
-    document.getElementById("pi2-r-imag-upper").value = xmax;
+    $("#pi2-r-imag-lower").val(xmin.toFixed(2));
+    $("#pi2-r-imag-upper").val(xmax.toFixed(2));
 
 }
 
 function pi2_firstcalcu() {
-    var l = document.getElementById("pi2-l").value / 1000000.0;
-    var c3 = 0.01 / (2 * 3.1415926535 * document.getElementById("pi2-work-freq").value * 1000000.0);
-    pi2_calculateRRange(l, c3);
+    let l = $("#pi2-l").val() / 1000000.0;
+    let c3 = 0.01 / (2 * 3.1415926535 * $("#pi2-work-freq").val() * 1000000.0);
+    pi2_calculateRRange(l);
 }
 
 function pi2_recalcu() {
-    var l = document.getElementById("pi2-l-reset").value / 1000000.0;
-    var c3 = document.getElementById("pi2-c3-reset").value * 1000000.0;
+    let l = $("#pi2-l-reset").val() / 1000000.0;
+    let c3 = $("#pi2-c3-reset").val() * 1000000.0;
     calculateRRange(l, c3);
 }
 
 function pi2_calculateC() {
-    var pi = 3.1415926535;
+    let c3 = $("#pi2-c3-reset").val() / 1000000000000;
+    let l = parseFloat($("#pi2-l-reset").val());
+    let f = $("#pi2-work-freq").val() * 1000000;
+    let w = 2 * pi * f;
+    let zzr = parseFloat($("#pi2-z-real").val());
+    let zzi = parseFloat($("#pi2-z-imag").val());
+    let zload = new Complex(zzr, zzi);
+    let a = new Complex(1, 0);
+    let yload = complexDivide(a, zload);
+    let yload1 = new Complex(yload.r, yload.i + w * c3);
+    let zload1 = complexDivide(a, yload1);
 
-    var c3 = document.getElementById("pi2-c3-reset").value / 1000000000000;
-    var l = document.getElementById("pi2-l-reset").value;
-    var f = document.getElementById("pi2-work-freq").value * 1000000;
-    var w = 2 * pi * f;
-    var zzr = document.getElementById("pi2-z-real").value;
-    var zzi = document.getElementById("pi2-z-imag").value;
-    var zload = new Complex();
-    zload.r = zzr;
-    zload.i = zzi;
-    var a = new Complex(1, 0);
-    var yload = complexDivide(a, zload);
-    var yload1 = new Complex();
-    yload1.r = yload.r;
-    yload1.i = yload.i + w * c3;
-    var zload1 = complexDivide(a, yload1);
+    let zloadg = new Complex(zload1.r, Math.sqrt(zload1.r * 50 - zload1.r * zload1.r));
+    let yloadg = new Complex(0.02, complexDivide(a, zloadg).i);
 
+    let c1 = -yloadg.i / (2 * pi * f) * 1000000000000;
+    let c2 = 1 / (zload1.i - zloadg.i + 2 * pi * f * l / 1000000) / (2 * pi * f) * 1000000000000;
 
-    var zloadg = new Complex(zload1.r, Math.sqrt(zload1.r * 50 - zload1.r * zload1.r));
-    var yloadg = new Complex(0.02, complexDivide(a, zloadg).i);
-
-    var c1 = -yloadg.i / (2 * pi * f) * 1000000000000;
-    var c2 = 1 / (zload1.i - zloadg.i + 2 * pi * f * l / 1000000) / (2 * pi * f) * 1000000000000;
-
-    document.getElementById("pi2-c1-2").value = c1;
-    document.getElementById("pi2-c2-2").value = c2;
+    $("#pi2-c1-2").val(c1.toFixed(2));
+    $("#pi2-c2-2").val(c2.toFixed(2));
 
 }
